7388d15ee0c3434d0a6c9f8911509a15131a9b60
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-20 21:07:40 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 module Memorable
-  module ClassMethods
-  def self.reset_all
-    self.all.clear
+    module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end
   end
-
-  def self.count
-    self.all.count
-  end
-end
 end
 
