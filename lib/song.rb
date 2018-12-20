3887f0df3b9852fe51124d429d530602c65cb0ed
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-20 21:20:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-  extend Findable::ClassMethods
 end
 
 class Song
@@ -15,9 +14,9 @@ class Song
     @@songs << self
   end
 
- #def self.find_by_name(name)
- #  @@songs.detect{|a| a.name == name}
- #end
+ def self.find_by_name(name)
+  @@songs.detect{|a| a.name == name}
+ end
 
   def self.all
     @@songs
